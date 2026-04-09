f7feb43f6a96bf9d91c4483ed03efbe43919b0f8
jamydv <127085144+iamjmydv@users.noreply.github.com>
2026-04-09 16:43:47 +0800

    added comment for steps using ConsumerWidget

--- riverpod_practice/lib/practice_provider/using_ConsumerWidget/using_consumer_widget_page.dart
+++ riverpod_practice/lib/practice_provider/using_ConsumerWidget/using_consumer_widget_page.dart
@@ -6,12 +6,18 @@ import 'package:riverpod_practice/practice_provider/provider/consumer_widget_pro
 /// our widget's build method gets an extra ref object (of type [WidgetRef]) that we can use to watch our provider.
 ///Using ConsumerWidget is the most common option and the one you should choose most of the time.
 ///
+///
+
+// 1. widget class now extends [ConsumerWidget]
 class UsingConsumerWidgetPage extends ConsumerWidget {
   const UsingConsumerWidgetPage({super.key});
 
   @override
+  // 2. build method has an extra [WidgetRef] argument
   Widget build(BuildContext context, WidgetRef ref) {
+    // 3. use ref.watch() to get the value of the provider
     final text = ref.watch(helloWorld);
+
     return Scaffold(
       body: Center(
         child: Text(
