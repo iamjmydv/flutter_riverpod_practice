8e02dd40d7990c67eecd891568b3055f0ffab73d
jamydv <127085144+iamjmydv@users.noreply.github.com>
2026-04-09 15:33:57 +0800

    added comment

--- riverpod_practice/lib/practice_provider/using_ConsumerWidget/page/using_consumer_widget_page.dart
+++ riverpod_practice/lib/practice_provider/using_ConsumerWidget/page/using_consumer_widget_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/practice_provider/using_ConsumerWidget/provider/consumer_widget_provider.dart';
 
+/// By subclassing [ConsumerWidget] instead of [StatelessWidget],
+/// our widget's build method gets an extra ref object (of type [WidgetRef]) that we can use to watch our provider.
+///Using ConsumerWidget is the most common option and the one you should choose most of the time.
+///
 class UsingConsumerWidgetPage extends ConsumerWidget {
   const UsingConsumerWidgetPage({super.key});
 
