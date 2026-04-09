31c13a25ba7782d5acf044a8c85ccbd93ddc89b7
jamydv <127085144+iamjmydv@users.noreply.github.com>
2026-04-09 15:31:18 +0800

    Added sample practice for ConsumerWidget

--- riverpod_practice/lib/practice_provider/using_ConsumerWidget/page/using_consumer_widget_page.dart
+++ riverpod_practice/lib/practice_provider/using_ConsumerWidget/page/using_consumer_widget_page.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_practice/practice_provider/using_ConsumerWidget/provider/consumer_widget_provider.dart';
 
 class UsingConsumerWidgetPage extends ConsumerWidget {
   const UsingConsumerWidgetPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final text = ref.watch(helloWorld);
     return Scaffold(
       body: Center(
-        child: Text('data'),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 48),
+        ),
       ),
     );
   }
---
+++ riverpod_practice/lib/practice_provider/using_ConsumerWidget/provider/consumer_widget_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final helloWorld = Provider<String>((ref) {
+  return 'Hello World!';
+});
